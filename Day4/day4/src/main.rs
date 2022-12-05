2e76b41b158f3a56431062e983a1f3201f5bf057
Justine <justine@squi.fr>
2022-12-05 18:15:08 +0100

    Day4_part2

--- Day4/day4/src/main.rs
+++ Day4/day4/src/main.rs
@@ -30,6 +30,27 @@ fn compute_line(line: String) -> bool {
     }
 }
 
+fn compute_line_p2(line: String) -> bool {
+    let mut s = line.split(",");
+
+    //Splitting our elves into vectors of i32...
+    //https://stackoverflow.com/questions/34090639/how-do-i-convert-a-vector-of-strings-to-a-vector-of-integers-in-a-functional-way
+    let mut elf1: Result<Vec<i32>, _> = s.next().expect("blah").split("-").map(|x| x.parse()).collect(); 
+    let mut elf2: Result<Vec<i32>, _> = s.next().expect("blah").split("-").map(|x| x.parse()).collect();
+
+    let elf1 = elf1.unwrap();
+    let elf2 = elf2.unwrap();
+    //println!("{:?} - {:?}", elf1, elf2);
+    //println!("{:?} - {:?}", range_elf1, range_elf2);
+
+    //That means they don't match...
+    if (elf2.first() > elf1.last() || elf1.first() > elf2.last()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 //Read the file line by line;
 //Sends OK unless one of our ? sends an error
 fn read_lines() -> io::Result<()> {
@@ -40,7 +61,7 @@ fn read_lines() -> io::Result<()> {
 
     for line in reader {
         let current_line = line?;
-        if compute_line(current_line) {
+        if compute_line_p2(current_line) {
             total = total + 1;
         };
     }
